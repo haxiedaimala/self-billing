197031cddd3b2911c8d535da21715b54d10e4b2d
myself <1907312518@qq.com>
2023-03-05 15:47:57 +0800

    将底部导航配置在 path:'/'的默认页面 Billing.vue 中

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,5 @@
 <template>
   <router-view/>
-  <hr>
-  <nav>
-    <router-link to="/details">明细</router-link>
-    |
-    <router-link to="/billing">账单</router-link>
-    |
-    <router-link to="/money">记账</router-link>
-    |
-    <router-link to="/category">类别</router-link>
-    |
-    <router-link to="/statistics">统计</router-link>
-    |
-  </nav>
 </template>
 
 <style lang="scss">
--- src/views/Billing.vue
+++ src/views/Billing.vue
@@ -3,6 +3,19 @@
 
 <template>
   <div>Billing.vue</div>
+  <hr>
+  <nav>
+    <router-link to="/details">明细</router-link>
+    |
+    <router-link to="/billing">账单</router-link>
+    |
+    <router-link to="/money">记账</router-link>
+    |
+    <router-link to="/category">类别</router-link>
+    |
+    <router-link to="/statistics">统计</router-link>
+    |
+  </nav>
 </template>
 
 <style lang="scss" scoped>
