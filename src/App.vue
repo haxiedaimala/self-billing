bd2e05a5d3d645beb9636822124af082f568992c
myself <1907312518@qq.com>
2023-04-21 19:44:38 +0800

    一键部署github文件，并添提示用户扫描二维码

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,26 @@
 <script setup lang="ts">
+import {onBeforeMount, ref} from 'vue';
+
+const width = ref(document.documentElement.clientWidth || document.body.clientWidth);
+const isShow = ref(width.value > 500);
+onBeforeMount(() => {
+  window.onresize = () => {
+    width.value = document.documentElement.clientWidth || document.body.clientWidth;
+    isShow.value = width.value > 500;
+  };
+});
+
+function onClose() {
+  isShow.value = !isShow.value;
+}
 </script>
 
 <template>
   <router-view/>
+  <div class="preview" v-show="isShow">
+    <img src="./assets/ercode.png" alt="手机预览" title="扫描二维码">
+    <span class="close" @click="onClose"/>
+  </div>
 </template>
 
 <style lang="scss">
@@ -27,4 +45,44 @@
   -webkit-tap-highlight-color: transparent;
 }
 
+.preview {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 200px;
+  height: 200px;
+
+  img {
+    position: relative;
+    width: 100%;
+  }
+
+  .close {
+    position: absolute;
+    top: -0.8em;
+    right: -0.8em;
+    width: 2em;
+    height: 2em;
+    border-radius: 50%;
+    background-color: rgba(0, 0, 0, .1);
+    cursor: pointer;
+
+    &::after,
+    &::before {
+      content: '';
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%) rotate(45deg);
+      background-color: grey;
+      width: 1em;
+      height: 1px;
+    }
+
+    &::before {
+      transform: translate(-50%, -50%) rotate(135deg);
+    }
+  }
+}
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const {defineConfig} = require('@vue/cli-service')
 const path = require('path')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/self-billing-website/'
+    : '/',
   transpileDependencies: true,
   lintOnSave: false,
 
